9d9a518ce1e553e31d14faf66eef4dde89518c73
Jitendra Singh <dba.jitendra@gmail.com>
2020-12-14 14:22:25 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,13 @@
+# simpleCode
+variable "REGION" {}
+variable "ACCESS_KEY" {}
+variable "SECRET_KEY" {}
+variable "Plan_Name" {}
+variable "Rule_Name" {}
+variable "VAULT_NAME" {}
+
+provider "aws" {
+  region     = var.REGION
+  access_key = var.ACCESS_KEY
+  secret_key = var.SECRET_KEY
+  }
