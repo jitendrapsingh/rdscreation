c97be81e7efecd019ad24808ba077fe13fdd202a
Jitendra Singh <dba.jitendra@gmail.com>
2020-12-18 11:38:01 +0530

    Added the selection & IAM Role Parameter

--- main.tf
+++ main.tf
@@ -21,3 +21,41 @@ resource "aws_backup_plan" "plan1" {
   }
 
 }
+##Added the selection & IAM Role Parameter.
+
+resource "aws_iam_role" "IAM_Role" {
+  name               = var.IAM_Role
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": ["sts:AssumeRole"],
+      "Effect": "allow",
+      "Principal": {
+        "Service": ["backup.amazonaws.com"]
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "IAM_ROle" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+  role       = aws_iam_role.IAM_Role.name
+}
+
+
+
+resource "aws_backup_selection" "selection1" {
+  iam_role_arn = aws_iam_role.IAM_Role.arn
+  name         = "tf_IAM_Role_backup_selection"
+  plan_id      = aws_backup_plan.plan1.id
+
+  selection_tag {
+    type  = "STRINGEQUALS"
+    key   = "Name"
+    value = "JitendraLinuxTest1"
+  }
+}
