df069d911a7f0623ddc9042f7a6d542517c99a90
Jitendra Singh <dba.jitendra@gmail.com>
2021-01-28 15:52:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_db_instance" "db_from_snapshot" {
   #region = var.REGION
   instance_class   = var.instance_type
   identifier              = var.DB_Name
+  name                    = var.DB1_Name
   #rds_license_model    = var.license_model
   db_subnet_group_name = var.subnet
   snapshot_identifier  = var.snapshot_identifier
