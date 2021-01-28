f01541dc8f4c932923c655d5c2874736bf52f885
Jitendra Singh <dba.jitendra@gmail.com>
2021-01-28 13:27:53 +0530

    Added the size

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "db_from_snapshot" {
   db_subnet_group_name = var.subnet
   snapshot_identifier  = var.snapshot_identifier
   vpc_security_group_ids = [ "${var.db_SG}" ]
-  allocated_storage = 25
+  allocated_storage = var.size
   skip_final_snapshot = false
   allow_major_version_upgrade = false
   auto_minor_version_upgrade = false
