b89885ae39506acba8fb940b6bfd9d4cd81c21d2
Jitendra Singh <dba.jitendra@gmail.com>
2021-01-28 13:29:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "db_from_snapshot" {
   db_subnet_group_name = var.subnet
   snapshot_identifier  = var.snapshot_identifier
   vpc_security_group_ids = [ "${var.db_SG}" ]
-  allocated_storage = var.size
+  allocated_storage = var.allocated_storage 
   skip_final_snapshot = false
   allow_major_version_upgrade = false
   auto_minor_version_upgrade = false
